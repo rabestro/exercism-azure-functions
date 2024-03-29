39f5db213677ff7c626199126ad2e7772d085b00
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-03-29 23:10:15 +0200

    Add Azure Functions infrastructure and ArmstrongNumbersFunction
    This commit adds all necessary files to support Azure Functions in the application. It also introduces the ArmstrongNumbersFunction, an HTTP trigger function that processes a request and outputs whether the provided numerical input is an Armstrong number or not. New configurations and settings relevant to Azure Functions and the ArmstrongNumbersFunction were also included in the project.

---
+++ Solution/ArmstrongNumbersFunction.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Exercism.Solution
+{
+    public class ArmstrongNumbersFunction(ILogger<ArmstrongNumbersFunction> logger)
+    {
+        [Function("armstrong-numbers")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var numberString = req.Query["number"];
+
+            if (string.IsNullOrEmpty(numberString) || !long.TryParse(numberString, out var number))
+            {
+                return new BadRequestObjectResult(new { message = "Please pass a number on the query string" });
+            }
+
+            var isArmstrongNumber = IsArmstrongNumber(number);
+
+            return new OkObjectResult(new { isArmstrongNumber });
+        }
+
+        private static bool IsArmstrongNumber(long number)
+        {
+            var exponent = number.ToString().Length;
+            var sumOfDigits = number.ToString()
+                .ToCharArray()
+                .Sum(digit => (long)Math.Pow(int.Parse(digit.ToString()), exponent));
+
+            return sumOfDigits == number;
+        }
+    }
+}
